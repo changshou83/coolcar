b03e0109492a0a5d7da32eb53a36cc89186b2073
长寿 <1369548156@qq.com>
2023-02-21 11:45:50 +0800

    feat: 首页视图完成

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -1,49 +1,162 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
+interface Marker {
+  iconPath: string;
+  id: number;
+  latitude: number;
+  longitude: number;
+  width: number;
+  height: number;
+}
+
+const defaultAvatar = '/resources/car.png'
+const initialLat = 42.05297
+const initialLng = 123.52658
 
 Page({
+  /* 视图所用数据 */
+  isPageShowing: false,
+  socket: undefined as WechatMiniprogram.SocketTask | undefined,
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    avatarURL: '',
+    scale: 16,
+    location: {
+      latitude: initialLat,
+      longitude: initialLng,
+    },
+    setting: {
+      skew: 0,
+      rotate: 0,
+      showLocation: true,
+      showScale: true,
+      subKey: '',
+      layerStyle: -1,
+      enableZoom: true,
+      enableScroll: true,
+      enableRotate: false,
+      showCompass: false,
+      enable3D: false,
+      enableOverlooking: false,
+      enableSatellite: false,
+      enableTraffic: false,
+    },
+    markers: [] as Marker[]
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
+  /* 生命周期 */
+  onLoad() {
+    const { globalData: { userInfo } } = getApp<IAppOption>()
+    this.setData({
+      avatarURL: userInfo?.avatarUrl
     })
   },
-  onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
+  onShow() {
+    this.isPageShowing = true;
+    if (!this.socket) {
+      this.setData({ markers: [] }, () => this.setupCarPosUpdater())
+    }
+  },
+  onHide() {
+    this.isPageShowing = false;
+    if (this.socket) {
+      this.socket.close({
+        success: () => (this.socket = undefined)
       })
     }
   },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+  setupCarPosUpdater() {
+    // get map context
+    // const mapCtx = wx.createMapContext("map");
+    // const markers = new Map<string, Marker>();
+    // lock
+    // let translating = false;
+    // const endTranslation = () => translating = false;
+    // const updateMarker = (fn: () => void) => {
+    //   translating = true;
+    //   fn()
+    // }
+    // get/create marker -> move marker
+    // TODO: CarService
+    // this.socket = CarService.subscribe(({ id, car }) => {
+    //   if (!id || translating || !this.isPageShowing) {
+    //     console.log("dropped");
+    //     return
+    //   }
+      
+    //   const { driver, position } = car;
+    //   const newIcon = driver.avatarUrl || defaultAvatar;
+    //   const newLat = position.latitude || initialLat;
+    //   const newLng = position.longitude || initialLng;
+    //   const marker = markers.get(id);
+    //   // create new marker
+    //   if(!marker) {
+    //     const { markers: _markers } = this.data;
+    //     const newMarker: Marker = {
+    //       id: _markers.length,
+    //       iconPath: newIcon,
+    //       latitude: newLat,
+    //       longitude: newLng,
+    //       height: 20,
+    //       width: 20
+    //     };
+    //     // insert new marker
+    //     markers.set(id, newMarker);
+    //     _markers.push(newMarker);
+    //     // update view
+    //     updateMarker(() => this.setData({ markers: this.data.markers }, endTranslation))
+    //     return
+    //   }
+
+    //   // Change Icon
+    //   if (marker.iconPath !== newIcon) {
+    //     marker.iconPath = newIcon;
+    //     marker.latitude = newLat;
+    //     marker.longitude = newLng;
+
+    //     updateMarker(() => this.setData({ markers: this.data.markers }, endTranslation))
+    //     return
+    //   }
+    //   // Move Marker
+    //   if (marker.latitude !== newLat || marker.longitude !== newLng) {
+    //     updateMarker(() => mapCtx.translateMarker({
+    //       markerId: marker.id,
+    //       destination: {
+    //         latitude: newLat,
+    //         longitude: newLng,
+    //       },
+    //       autoRotate: false,
+    //       rotate: 0,
+    //       duration: 80,
+    //       animationEnd: endTranslation,
+    //     }))
+    //   }
+    // })
+  },
+  /* 以下为视图所用方法 */
+  // 跳转到我的行程
+  gotoTrips() {
+    // TODO
+  },
+  // 定位当前位置
+  locateCurLoc() {
+    wx.getLocation({
+      type: 'gcj02', // 返回可用于 wx.openLocation 的坐标
       success: (res) => {
-        console.log(res)
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          }
+        })
+      },
+      fail() {
+        // 提示用户
+        wx.showToast({
+          icon: 'none',
+          title: '请前往设置页授权'
         })
       }
     })
   },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  // 扫码
+  async scanCode() {
+    // TODO
   }
 })
